49626ff25d6d8eb48c80dc7b42763808837f4ff9
liensanchez <liensilviosanchez@gmail.com>
2023-03-11 02:02:19 -0300

    connected back & front end fetch petition

--- client/index.js
+++ client/index.js
@@ -1,7 +1,51 @@
-let button = document.getElementById('sendButton')
+let button = document.getElementById('sendBuy')
 
-button.addEventListener('click', funca)
+button.addEventListener('click', buy)
 
-function funca () {
-  console.log('funca')
+const data = {
+  "id_product": 1234,
+    "title": "mercadopago certificacion",
+    "price": 2000,
+    "email":"liensanchezppc@gmail.com",
+    "quantity": 1,
+    "picture": "https://agustinhansen.com/wp-content/uploads/2020/10/mercado-pago-peru.jpg",
+    "description": "BackEnd for Certification",
+    "category": "BackEnd",
+    "name": "Lien",
+    "lastName": "Sanchez",
+    "area": "261",
+    "phone": 2167506,
+    "street": "jujuy",
+    "numberStreet": 1910,
+    "postal": "5506"
 }
+
+function buy () {
+  fetch('http://localhost:3010/api/buy', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+    
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error))
+}
+
+
+
+/* 
+  fetch('http://localhost:3010/api/buy/pendig', {
+    method: 'GET',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => {
+    console.log(error)
+  })
+*/
