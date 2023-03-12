8a56c1fa25411c34cc3e55bc712c0c1bb0f0bddb
liensanchez <liensilviosanchez@gmail.com>
2023-03-12 01:03:30 -0300

    NEW KEYS

--- api/src/libs/mercadopago/mercadopago.js
+++ api/src/libs/mercadopago/mercadopago.js
@@ -1,14 +1,14 @@
 const mercadopago = require("mercadopago");
 require('dotenv').config();
 const {
-  ACCES_TOKEN
+  ACCES_TOKEN_TEST
 } = process.env
 
 function mercadopagoconfig() {
   
   // Agrega credenciales
   mercadopago.configure({
-    access_token: ACCES_TOKEN
+    access_token: ACCES_TOKEN_TEST
   });
   
 }
